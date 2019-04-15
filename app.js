ff22f52c22e5a59f87d5073754355ea34c456e08
Jacob  Clarke <jacobclarke@Jacobs-MacBook-Pro.local>
2019-04-15 15:30:44 -0400

    working code needs styling

---
+++ app.js
@@ -0,0 +1,134 @@
+$(() => {
+
+      $('.ike').hide();
+      $('.DeVaughn').hide();
+      $('.carmelo').hide();
+      $('.baker').hide();
+      $('.jabari').hide();
+
+const players = []
+
+
+
+  $('.generate').on('click', () => {
+    $('ul').empty();
+    const randomPlayers = players[Math.floor(Math.random() * players.length)];
+    // console.log(randomPlayers);
+
+    const $li = $('<li>')
+    $li.append(randomPlayers)
+    $('.players').append($li)
+
+    if(randomPlayers == 'Ike Anigbogu'){
+      $('.ike').show();
+    } else {
+      $('.ike').hide();
+    }
+    if(randomPlayers == 'DeVaughn Akoon-Purcell'){
+      $('.DeVaughn').show();
+    } else {
+      $('.DeVaughn').hide();
+    }
+    if(randomPlayers == 'Carmelo Anthony'){
+      $('.carmelo').show();
+    } else {
+      $('.carmelo').hide();
+    }
+    if(randomPlayers == 'Ron Baker'){
+      $('.baker').show();
+    } else {
+      $('.baker').hide();
+    }
+    if(randomPlayers == 'Jabari Bird'){
+      $('.jabari').show();
+    } else {
+      $('.jabari').hide();
+    }
+
+
+  })
+
+  let currentImgIndex = 0;
+  let currentNumOfImages = $('.player-picture').children().length - 1;
+
+
+  $('.next').on('click', () => {
+    $('ul').empty();
+    $('.player-picture').children().eq(currentImgIndex).hide();
+    if (currentImgIndex < currentNumOfImages) {
+      currentImgIndex++
+    } else {
+      currentImgIndex = 0
+    }
+    $('.player-picture').children().eq(currentImgIndex).show();
+
+    playerName();
+  })
+
+  $('.previous').on('click', () => {
+    $('ul').empty();
+    $('.player-picture').children().eq(currentImgIndex).hide();
+    if (currentImgIndex > 0) {
+      currentImgIndex--;
+    } else {
+      currentImgIndex = currentNumOfImages
+    }
+
+    $('.player-picture').children().eq(currentImgIndex).show();
+
+    playerName();
+  })
+
+  const playerName = () => {
+    if(currentImgIndex == 0){
+      const $li = $('<li>')
+      $li.append('Ike Anigbogu');
+      $('.players').append($li)
+    }
+    if(currentImgIndex == 1){
+      const $li = $('<li>')
+      $li.append('DeVaughn Akoon-Purcell');
+      $('.players').append($li)
+    }
+    if(currentImgIndex == 2){
+      const $li = $('<li>')
+      $li.append('Carmelo Anthony');
+      $('.players').append($li)
+    }
+    if(currentImgIndex == 3){
+      const $li = $('<li>')
+      $li.append('Ron Baker');
+      $('.players').append($li)
+    }
+    if(currentImgIndex == 4){
+      const $li = $('<li>')
+      $li.append('Jabari Bird');
+      $('.players').append($li)
+    }
+
+  }
+
+
+$.ajax(
+  {
+    url: 'https://www.balldontlie.io/api/v1/players?per_page=5',
+    success: (data) => {
+      for(let i = 0; i < data.data.length; i ++){
+      if(data.data[i].team.full_name){
+
+        // const $li = $('<li>')
+        // $li.append(data.data[i].first_name)
+        // $('.name').append($li)
+
+        players.push(data.data[i].first_name + ' ' + data.data[i].last_name)
+        // players.push(data.data[i].last_name)
+
+        // $li.append(data.data[i].last_name)
+        // $('.name').append($li)
+        }
+      }
+      // console.log(data.data);
+    }
+  }
+)
+})
